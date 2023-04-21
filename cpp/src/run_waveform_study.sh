2d145dcd73e8e1f1e1e6f37fe2a39a4a20e78aba
Andrew Mogan <amogan@vols.utk.edu>
2023-04-21 10:58:51 -0500

    Move data files to new directory

--- cpp/src/run_waveform_study.sh
+++ cpp/src/run_waveform_study.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 DIRNAME="/icarus/data/users/amogan/cali_ntuples_for_diffusion/"
-FILETEXT="cali_ntuple_corsika_20kEvts_elifetime.root"
+FILETEXT="cali_ntuple_corsika_17kEvts_noiseoff.root"
 RUNTYPE="XX"
 PLANENUM=2
 PLOTNAME="plot.pdf"
