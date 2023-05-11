e3542f1f531501f67243d7761979999716e7acfc
Andrew Mogan <andrew.mogan@colostate.edu>
2023-05-11 09:28:17 -0700

    Add invariant3 functions from Lukas' paper

--- python/src/lardiff/test_statistics.py
+++ python/src/lardiff/test_statistics.py
@@ -2,6 +2,11 @@ import numpy as np
 from scipy import fftpack
 from scipy import stats
 
+from functools import lru_cache
+from scipy.stats import rv_continuous, chi2, ttest_ind, chisquare
+from scipy.special import erf, erfinv
+from scipy.optimize import root_scalar
+
 from .consts import *
 from .waveform_functions import smear_signal, convolve, \
      deconvolve, coarsen_signal, fix_baseline, shift_signal_1D
@@ -105,3 +110,73 @@ def calc_chisq(input_sig, anode_hist, anode_uncert_hist, cathode_hist, cathode_u
         numvals += min_numvals
                 
     return chisq, numvals, shift_vec
+
+### Invariant3 functions as given in 
+### https://journals.aps.org/prd/abstract/10.1103/PhysRevD.103.113008 
+
+class Bee(rv_continuous):
+    def _cdf(self, x, df):
+        return erf(x/np.sqrt(2))**df
+    def _pdf(self, x, df):
+        ret = df*(erf(x/np.sqrt(2)))**(df-1)
+        return ret * np.sqrt(2/np.pi)*np.exp(-x**2/2)
+    def ppf(self, x, df):
+        return erfinv((x)**(1/df)) * np.sqrt(2)
+    
+class BeeSquared(rv_continuous):
+    def _cdf(self, x, df):
+        b = np.sqrt(x)
+        ret = bee.cdf(b, df)
+        return ret
+    def _pdf(self, x, df):
+        ret = df*(erf(np.sqrt(x/2)))**(df-1)
+        return ret / np.sqrt(2*np.pi*x) * np.exp(-x/2)
+    def ppf(self, x, df):
+        b = bee.ppf(x, df)
+        return b**2
+
+@np.vectorize
+@lru_cache(10000)
+def _yfrommax(b, df = 2, alpha = 0.5):
+    """(1—diagonal coordinate) from (1—max) of accepted region"""
+    # A=(1-b)**df—((y-b)**df)/((1-alpha+alpha*y)**(df-1))=1—y
+    beta = 1 - alpha
+    q=(1.0-b) ** df-1
+    dfm=df-1
+    def f(y):
+        return q-((y-b) ** df) / ((beta + alpha * y) ** (dfm)) + y
+    if b <=0:
+        return 0.0
+    if b >= 1:
+        return 1.0
+    else:
+        return root_scalar(f, x0=b, x1 = b * 1.001).root
+
+def yfrommax(b, df = 2, alpha = 0.5):
+    """Buffer and interpolate values to speed things up."""
+    step = 0.0001
+    b_= np.floor(b / step, dtype=float) * step
+    b__= b_ + step
+    delta = (b-b_) / step
+    x_  = _yfrommax(b_,  df = df, alpha = alpha)
+    x__ = _yfrommax(b__, df = df, alpha = alpha)
+    return x_+(x__ - x_) * delta
+
+def invariant3(x, alpha = 0.5, fast = False):
+    """Return test statistic given vector of normalized values."""
+    if fast:
+        sf = 1 - chi2.cdf(x ** 2, df = 1) # Faster, but less accurate
+    else:
+        sf = chi2.sf(x ** 2, df = 1)
+    # Get possible diagonal coordinate from maximum CDF value (= minimum SF)
+    a = np.min(sf, axis=-1)
+    b = np.max(sf, axis=-1)
+    yfm = yfrommax(a, df=x.shape[-1], alpha = alpha)
+    # Get possible diagonal coordinate from center surface
+    yfc = (alpha * (a - b) + b) / (1.0 + alpha * (a - b))
+    y = np.minimum(yfc, yfm)
+    y = np.maximum(y, 0) # Cap in case of rounding or root finding errors
+    return chi2.isf(y, df = 1)
+
+
+
