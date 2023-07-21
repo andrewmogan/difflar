Correct uncertainties and normalization

python/src/lardiff/test_statistics.py
@@ -50,7 +50,8 @@ def calc_test_statistic(anode_hist, anode_uncert_hist,
         )
     elif test_statistic == "invariant3_redux":
         temp_test_stat, temp_num_values, shift_vector = calc_invariant3_redux(
-            pred_hist, cathode_hist, cathode_max,
+            pred_hist, pred_uncert_hist,
+            cathode_hist, cathode_uncert_hist, cathode_max,
             interpolation
         )
     else:
@@ -214,11 +215,12 @@ def calc_invariant3_alt(pred_hist, cathode_hist, cathode_max, interpolation='sci
     return invar3, numvals, shift_vec
 
 # Yet another attempt at invariant3
-def calc_invariant3_redux(pred_hist, cathode_hist, cathode_max, interpolation='scipy'):
+def calc_invariant3_redux(pred_hist, pred_uncert_hist, cathode_hist, cathode_uncert_hist, cathode_max, interpolation='scipy'):
     invariant3_value = 0
     numvals = 0
     shift_vec = np.zeros((N_wires))
     z_scores = np.array([])
+    pred_uncert_hist_1D_shifted = np.empty_like(pred_uncert_hist)
 
     # Invariant3 function
     dist = lambda x: invariant3(x, alpha=2/3, fast=False)
@@ -240,32 +242,28 @@ def calc_invariant3_redux(pred_hist, cathode_hist, cathode_max, interpolation='s
 
         min_wire_z_scores = []
 
-        #for shift_val in shift_vals:
-        for shift_val in range(0, 1):
+        for shift_val in shift_vals:
+        #for shift_val in range(0, 1):
             pred_interp = interp1d(np.arange(N_ticks), pred_hist[:, col], fill_value='extrapolate', kind='cubic')
             pred_hist_1D_shifted[:, col] = pred_interp(np.arange(N_ticks) - shift_val)
 
-            sigma1 = np.std(pred_hist_1D_shifted)
-            sigma2 = np.std(cathode_hist)
-            sigma  = np.sqrt(sigma1 * sigma1 + sigma2 * sigma2)
+            uncert_interp = interp1d(np.arange(N_ticks), pred_uncert_hist[:, col], fill_value='extrapolate', kind='cubic')
+            pred_uncert_hist_1D_shifted[:, col] = uncert_interp(np.arange(N_ticks) - shift_val)
 
             cathode_mask = cathode_hist[N_ticks_start:N_ticks_end, col] >= threshold_rel * cathode_max
             pred_norm = pred_hist_1D_shifted[N_ticks_start:N_ticks_end, col][cathode_mask].sum()
             cathode_norm = cathode_hist[N_ticks_start:N_ticks_end, col][cathode_mask].sum()
 
-            prediction_roi = pred_hist_1D_shifted[N_ticks_start:N_ticks_end, col][cathode_mask]
-            measurement_roi = cathode_hist[N_ticks_start:N_ticks_end, col][cathode_mask]
-            wire_z_scores_temp = (prediction_roi / pred_norm - measurement_roi / cathode_norm) / sigma
-
-            print('wire_z_scores_temp', wire_z_scores_temp)
-            print('len wire_z_scores_temp:', len(wire_z_scores_temp))
+            prediction_roi = pred_hist_1D_shifted[N_ticks_start:N_ticks_end, col][cathode_mask] / pred_norm
+            measurement_roi = cathode_hist[N_ticks_start:N_ticks_end, col][cathode_mask] / cathode_norm
+            sigma_1 = pred_uncert_hist_1D_shifted[N_ticks_start:N_ticks_end, col][cathode_mask] / pred_norm
+            sigma_2 = cathode_uncert_hist[N_ticks_start:N_ticks_end, col][cathode_mask] / cathode_norm
+            sigma = np.sqrt(sigma_1 * sigma_1 + sigma_2 * sigma_2)
+            wire_z_scores_temp = (prediction_roi - measurement_roi) / sigma
 
             numvals_temp = np.count_nonzero(cathode_mask)
 
             distance_temp = dist(wire_z_scores_temp)
-            print('distance_temp :', distance_temp)
-            print('min_distance :', min_distance)
-            print('distance_temp < min_distance? :', distance_temp < min_distance)
 
             if distance_temp < min_distance:
                 min_distance = distance_temp